d93d43bafd19a12f13c5ea5019829e16bef8adff
kota <kmori05@gmail.com>
2017-04-17 10:28:07 +0900

    added help

--- testpack/greeting.py
+++ testpack/greeting.py
@@ -4,11 +4,21 @@ from pkg_resources import resource_string
 
 
 def say_hello():
+    """
+    Print 'Hello!' on concole
+    
+    :return: None
+    """
     print("Hello!")
 
 
 
 def give_quote():
+    """
+    Print a nice quote by Oscar Wilde
+    
+    :return: None
+    """ 
     x = resource_string(__name__, 'wilde.txt').decode().strip()
     print(x)
 
--- testpack/math.py
+++ testpack/math.py
@@ -5,10 +5,28 @@ from pkg_resources import resource_stream
 
 
 def sumproduct(x, y):
+    """
+    Compute the sum of elementwise product of two vectors
+    
+    :param x: vector
+    :param y: vector
+    :type x: list of numbers or 1-dim numpy.array
+    :type y: list of numbers of 1-dim numpy.array
+    :return: sum of elementwise product of x and y
+    :rtype: number
+    """
     return np.dot(np.array(x), np.array(y))
 
 
 def magic_square(n):
+    """
+    Return a magic square of specified size
+    
+    :param n: size of magic square
+    :type n: int
+    :return: magic square of size n if n is supported. Otherwise None
+    :rtype: 2-dim numpy.array of shape (n,n) if supported. Otherwise None
+    """
     if n in [3, 4]:
         with resource_stream(__name__, 'magic_square/%d.npy' % n) as f:
             x = np.load(f)
